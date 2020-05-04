0ea9877fbdcfacc22987a311ec418f6f625d801f
Dem4236 <onoderadesk0402@gmail.com>
2020-05-04 12:54:29 +0900

    fix

--- Project1/WinMain.cpp
+++ Project1/WinMain.cpp
@@ -1,4 +1,5 @@
 #include<Windows.h>
+#include"WindowsMessageMap.h"
 
 //ウィンドウプロシージャ メッセージ処理を行う
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
@@ -60,11 +61,43 @@ int CALLBACK WinMain(
 //wParam,lParamにはキーボードのコード、マウスカーソルの座標などが格納されている
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
+	//出力に送られているメッセージ情報を表示する
+	static WindowsMessageMap mm;
+	OutputDebugString(mm(msg, lParam, wParam).c_str());
+
 	switch (msg) {
 		//ウィンドウが閉じられたとき
 	case WM_CLOSE:
 		//WM_QUITを送る
 		PostQuitMessage(0);
+		break;
+	case WM_KEYDOWN:
+		//wParamに仮想キーコード情報が入る
+		if (wParam == 'F') {
+			SetWindowText(hWnd, "Respects");
+		}
+		break;
+	case WM_KEYUP:
+		if (wParam == 'F') {
+			SetWindowText(hWnd, "Dangerfield");
+		}
+		break;
+		//テキストなどで打ち込めるキーが押されたとき
+	case WM_CHAR:
+		//static std::string title;
+		//title.push_back((char)wParam);
+		//SetWindowText(hWnd, title.c_str());
+		//break;
+		//ウィンドウ内でマウスの左クリックが押されたとき
+	case WM_LBUTTONDOWN:
+		//lParamの下位16ビットにx座標,上位16ビットにy座標が入っている
+		//int x = LOWORD(lParam);
+		//int y = HIWORD(lParam);
+		//lParamの情報からx,yの情報を取得
+		POINTS pt = MAKEPOINTS(lParam);
+		char szBuf[256];
+		wsprintf(szBuf, TEXT("X %d : Y %d"), pt.x, pt.y);
+		SetWindowText(hWnd, szBuf);
 		break;
 	}
 	return DefWindowProc(hWnd, msg, wParam, lParam);
