dbfd8d620130ee43235642a26d5c087f55db6981
Dem4236 <onoderadesk0402@gmail.com>
2020-06-03 00:19:34 +0900

    Revert "28. リファクタリング"
    This reverts commit 313863bb913ae1378605f9018f71a9e1b53f31fa.

--- Project1/Mesh.h
+++ Project1/Mesh.h
@@ -25,7 +25,7 @@ class Node {
 public:
 	Node(const std::string& name,std::vector<Mesh*> meshPtrs, const DirectX::XMMATRIX& transform) noexcept;
 	void Draw(Graphics& gfx, DirectX::FXMMATRIX accumulatedTransform) const noexcept;
-	void ShowTree()const noexcept;
+	void RenderTree()const noexcept;
 private:
 	void AddChild(std::unique_ptr<Node> pChild) noexcept;
 private:
@@ -40,14 +40,20 @@ public:
 	Model(Graphics& gfx, const std::string fileName);
 	void Draw(Graphics& gfx) const;
 	void ShowWindow(const char* windowName = nullptr)noexcept;
-	~Model()noexcept;
 private:
 	static std::unique_ptr<Mesh> ParseMesh(Graphics& gfx, const aiMesh& mesh);
 	std::unique_ptr<Node> ParseNode(const aiNode& node)noexcept;
 private:
 	std::unique_ptr<Node> pRoot;
 	std::vector<std::unique_ptr<Mesh>> meshPtrs;
-	std::unique_ptr<class ModelWindow> pWindow;
+	struct {
+		float roll = 0.0f;
+		float pitch = 0.0f;
+		float yaw = 0.0f;
+		float x = 0.0f;
+		float y = 0.0f;
+		float z = 0.0f;
+	}position;
 };
 
 //
